564f28860ba493223155a0475502b783076802d7
DezyKode <dezykode@gmail.com>
2024-12-05 23:08:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,19 +5,19 @@ import App from './App';
 import app from './App.css'
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Header from './Header';
+import Footer from './Footer';
+// import Header from './Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+root.render( <React.StrictMode>
+   <Header/>
+    <App/>
+<Footer/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-
-
